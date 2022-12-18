2a69297623d108496029f65d3222a0669b3b9334
rasheedawilson12 <rasheedawilson12@gmail.com>
2022-12-18 11:27:27 -0600

    Current weather only transfer

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from "react";
+import Weather from "./Weather";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          This project was coded by Cedra Wilson and is{" "}
+          <a
+            href="https://github.com/rasheedawilson12/shecodes-react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          .
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/WeatherInfo.js
@@ -0,0 +1,37 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
+      <ul>
+        <li>
+          <FormattedDate date={props.data.date} />
+        </li>
+        <li className="text-capitalize">{props.data.description}</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex">
+            <div className="float-left">
+              <WeatherIcon code={props.data.icon} size={50} />
+            </div>
+            <div className="float=left">
+              <WeatherTemperature celcius={props.data.temperature} />
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {Math.round(props.data.wind)} km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
